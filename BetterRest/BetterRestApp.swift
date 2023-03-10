2b7fafd9e95f5b5bd232f9ea7232705c6aee6cb3
cesarlopezt <cesarlopez2808@gmail.com>
2023-03-10 17:56:09 -0600

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Cesar Lopez on 3/10/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
